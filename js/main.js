81b880fa11f689d7d641a9ca7a6928daa4d77e69
illya-onyshchuk <llayonys@gmail.com>
2021-08-07 22:01:14 +0300

    done block news

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
 
-$('.slider__inner').slick({
+$('.slider__inner, .news__slider-inner').slick({
   nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
   prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
   infinite: false,
@@ -8,5 +8,6 @@ $('.slider__inner').slick({
 });
 
 $('select').styler();
+
 
 });
